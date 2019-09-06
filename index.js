5073ec1a6bde59fd4e4279674886ee5333bcf717
knoma <knoma2010@gmail.com>
2019-09-06 10:10:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+var os = require('os');
+
 
 app.get('/', function (req, res) {
-  res.send('Hello master from maher');
+  res.send('Hello World from ' + os.hostname());
 });
 
 
