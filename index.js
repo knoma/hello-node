bce513db6e80fe2b69c027fcd06037970c5806f2
knoma <knoma2010@gmail.com>
2019-09-06 10:06:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var express = require('express');
-var os = require('os');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello master from ' + os.hostname());
+  res.send('Hello master from maher');
 });
 app.listen(8080, function () {
   console.log('app listening on port 8080!');
